92d555ae4cec6d9c61f5cb71c5d5e726309e6190
13byte <13bitadc@naver.com>
2025-07-16 13:39:38 +0900

    feat: Step Functions 워크플로우 설정
    - : 메인 Step Functions 상태 머신을 정의하고, 모든 Lambda 함수, Fargate 태스크, SageMaker 엔드포인트, DynamoDB 테이블 등 파이프라인 구성 요소를 연결.
    - CloudWatch Logs를 통한 실행 로깅을 설정하여 워크플로우 가시성을 확보.

--- infra/step_functions.tf
+++ infra/step_functions.tf
@@ -1,23 +1,23 @@
 resource "aws_sfn_state_machine" "book_scan_workflow" {
-  name       = "${var.project_name}-main-workflow"
-  role_arn   = aws_iam_role.step_functions_role.arn
+  name     = "${var.project_name}-main-workflow"
+  role_arn = aws_iam_role.step_functions_role.arn
   definition = templatefile("${path.module}/../step-functions/main-workflow.json", {
     trigger_pipeline_lambda_arn = aws_lambda_function.trigger_pipeline.arn
     initialize_state_lambda_arn = aws_lambda_function.initialize_state.arn
     orchestrator_lambda_arn     = aws_lambda_function.orchestrator.arn
-    
-    detect_skew_lambda_arn      = aws_lambda_function.detect_skew.arn
-    process_ocr_lambda_arn      = aws_lambda_function.process_ocr.arn
-    upscale_image_lambda_arn    = aws_lambda_function.upscaler.arn
-    fargate_task_arn            = aws_ecs_task_definition.skew_corrector.arn
+
+    detect_skew_lambda_arn   = aws_lambda_function.detect_skew.arn
+    process_ocr_lambda_arn   = aws_lambda_function.process_ocr.arn
+    upscale_image_lambda_arn = aws_lambda_function.upscaler.arn
+    fargate_task_arn         = aws_ecs_task_definition.skew_corrector.arn
 
     generate_pdf_lambda_arn         = aws_lambda_function.pdf_generator.arn
     generate_run_summary_lambda_arn = aws_lambda_function.summary_generator.arn
 
-    ecs_cluster_arn      = aws_ecs_cluster.main.arn
-    subnet_id            = aws_subnet.private[0].id
-    security_group_id    = aws_security_group.main.id
-    dynamodb_table_name  = aws_dynamodb_table.state_tracking.name
+    ecs_cluster_arn     = aws_ecs_cluster.main.arn
+    subnet_id           = aws_subnet.private[0].id
+    security_group_id   = aws_security_group.main.id
+    dynamodb_table_name = aws_dynamodb_table.state_tracking.name
   })
 
   logging_configuration {
