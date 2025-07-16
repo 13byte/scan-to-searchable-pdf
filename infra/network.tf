4f2ca795f713478c297072373fc73ce63749bf1d
13byte <13bitadc@naver.com>
2025-07-16 13:37:57 +0900

    feat: VPC 및 네트워크 인프라 설정
    - : VPC, 프라이빗 서브넷, 인터넷 게이트웨이, 라우팅 테이블, 보안 그룹을 정의.
    - S3, ECR, SageMaker, CloudWatch Logs, Secrets Manager, STS, Step Functions, DynamoDB, EventBridge, Lambda 등 주요 AWS 서비스에 대한 VPC 엔드포인트를 설정하여 보안 및 비용 효율성을 강화.

--- infra/network.tf
+++ infra/network.tf
@@ -66,8 +66,8 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = aws_vpc.main.id
-  service_name = "com.amazonaws.${var.aws_region}.s3"
+  vpc_id          = aws_vpc.main.id
+  service_name    = "com.amazonaws.${var.aws_region}.s3"
   route_table_ids = [aws_route_table.private.id]
 
   tags = {
