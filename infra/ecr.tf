3caeaa7ae802b24800d70b9800427811cd96b538
13byte <13bitadc@naver.com>
2025-07-16 13:35:34 +0900

    refactor: ECR 이미지 태그 불변성 설정 및 라이프사이클 정책 개선
    - : 모든 ECR 저장소의 를 에서 로 변경하여 이미지 태그의 일관성과 재현성을 보장.
    - : ECR 라이프사이클 정책에서 를 제거하여 모든 태그된 이미지에 정책이 적용되도록 개선.

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "fargate_processor" {
   name                 = "${var.project_name}/skew-corrector"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -9,7 +9,7 @@ resource "aws_ecr_repository" "fargate_processor" {
 
 resource "aws_ecr_repository" "sagemaker_realesrgan" {
   name                 = "${var.project_name}/sagemaker-realesrgan"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -18,7 +18,7 @@ resource "aws_ecr_repository" "sagemaker_realesrgan" {
 
 resource "aws_ecr_repository" "detect_skew_lambda" {
   name                 = "${var.project_name}/detect-skew"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -27,7 +27,7 @@ resource "aws_ecr_repository" "detect_skew_lambda" {
 
 resource "aws_ecr_repository" "process_ocr_lambda" {
   name                 = "${var.project_name}/process-ocr"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -36,7 +36,7 @@ resource "aws_ecr_repository" "process_ocr_lambda" {
 
 resource "aws_ecr_repository" "trigger_pipeline_lambda" {
   name                 = "${var.project_name}/trigger-pipeline"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -45,10 +45,10 @@ resource "aws_ecr_repository" "trigger_pipeline_lambda" {
 
 resource "aws_ecr_lifecycle_policy" "default_policy" {
   for_each = {
-    fargate   = aws_ecr_repository.fargate_processor.name
-    sagemaker = aws_ecr_repository.sagemaker_realesrgan.name
-    detect_skew = aws_ecr_repository.detect_skew_lambda.name
-    process_ocr = aws_ecr_repository.process_ocr_lambda.name
+    fargate          = aws_ecr_repository.fargate_processor.name
+    sagemaker        = aws_ecr_repository.sagemaker_realesrgan.name
+    detect_skew      = aws_ecr_repository.detect_skew_lambda.name
+    process_ocr      = aws_ecr_repository.process_ocr_lambda.name
     trigger_pipeline = aws_ecr_repository.trigger_pipeline_lambda.name
   }
   repository = each.value
@@ -60,7 +60,6 @@ resource "aws_ecr_lifecycle_policy" "default_policy" {
         description  = "최근 3개의 태그된 이미지 ��지",
         selection = {
           tagStatus     = "tagged",
-          tagPrefixList = ["v"],
           countType     = "imageCountMoreThan",
           countNumber   = 3
         },
