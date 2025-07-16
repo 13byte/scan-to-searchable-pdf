2943b41a1f0155f1a5777568cece9172e2b18589
13byte <13bitadc@naver.com>
2025-07-16 13:38:55 +0900

    feat: Secrets Manager를 통한 Google Vision API 자격 증명 관리
    - : Google Cloud Vision API 서비스 계정 자격 증명을 Secrets Manager에 안전하게 저장하도록 설정.
    -  라이프사이클 규칙을 사용하여 Terraform이 의 변경을 무시하도록 하여 수동 업데이트를 지원.

--- infra/secretsmanager.tf
+++ infra/secretsmanager.tf
@@ -1,21 +1,21 @@
 resource "aws_secretsmanager_secret" "google_credentials" {
-  name = "${var.project_name}-google-vision-credentials"
+  name        = "${var.project_name}-google-vision-credentials"
   description = "Google Cloud Vision API JSON 자격 증명을 저장합니다."
 }
 
 resource "aws_secretsmanager_secret_version" "google_credentials" {
-  secret_id     = aws_secretsmanager_secret.google_credentials.id
+  secret_id = aws_secretsmanager_secret.google_credentials.id
   secret_string = jsonencode({
-    "type": "service_account",
-    "project_id": "your-gcp-project-id",
-    "private_key_id": "your-private-key-id",
-    "private_key": "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n",
-    "client_email": "your-service-account-email",
-    "client_id": "your-client-id",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "..."
+    "type" : "service_account",
+    "project_id" : "your-gcp-project-id",
+    "private_key_id" : "your-private-key-id",
+    "private_key" : "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n",
+    "client_email" : "your-service-account-email",
+    "client_id" : "your-client-id",
+    "auth_uri" : "https://accounts.google.com/o/oauth2/auth",
+    "token_uri" : "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url" : "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url" : "..."
   })
 
   lifecycle {
